2384e5ef273b8e8a5ecb94ea0082111e6e0f36fd
Greg <xyi7i@yandex.ru>
2022-04-25 13:02:35 +0300

    GeekBrains

--- Geek/C#/lesson1/HW/task3/Program.cs
+++ Geek/C#/lesson1/HW/task3/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Задача 3. Программа на вход принимает число и выдаёт, является ли число чётным.");
+
+Console.Write("Введите число: ");
+int numa = int.Parse(Console.ReadLine ());
+// int test = numa % 2;
+if (numa % 2 == 0)
+{
+    Console.WriteLine("число чётноё");
+}
+else
+{
+    Console.WriteLine("число нечётноё");
+}
