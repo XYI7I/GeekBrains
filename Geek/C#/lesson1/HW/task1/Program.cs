f9b1137c43b6e5fd23524aab716137a7bf5fc84c
Greg <xyi7i@yandex.ru>
2022-04-25 12:46:14 +0300

    GeekBrains

---
+++ Geek/C#/lesson1/HW/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Задание 1. Программа, которая принимает два числа и выдаёт какое большее, а какое меньшее.");
+
+Console.Write("Первое число: ");
+int numa = int.Parse(Console.ReadLine ());
+
+Console.Write("Второе число: ");
+int numb = int.Parse(Console.ReadLine ());
+
+if (numa > numb)
+{
+    Console.WriteLine("Первое число: " + numa + " больше второгочисла: " + numb);
+}
+
+if (numa == numb)
+{
+    Console.WriteLine("Числа равны!");
+}
+
+if (numa < numb)
+{
+    Console.WriteLine("Первое число: " + numa + " меньше второго числа: " + numb);
+}
---
+++ Geek/C#/lesson1/HW/task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Задание 2. Программа принимает на вход три числа и выдаёт максимальное из этих чисел.");
+
+Console.Write("Первое число: ");
+int numa = int.Parse(Console.ReadLine ());
+
+Console.Write("Второе число: ");
+int numb = int.Parse(Console.ReadLine ());
+
+Console.Write("Третье число: ");
+int numc = int.Parse(Console.ReadLine ());
+
+int maxnum = numa;
+
+if (numb > maxnum)
+{
+    maxnum = numb;
+}
+
+if (numc > maxnum)
+{
+    maxnum = numc;
+}
+
+Console.WriteLine("Максимальное число: " + maxnum);
