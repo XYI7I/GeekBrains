cd7f306f1617075568a3c427f8e1dcf2e40338a7
Greg <xyi7i@yandex.ru>
2022-04-25 21:27:55 +0300

    GeekBrains

---
+++ Geek/C#/lesson1/HW/Task_with_dog.py
@@ -0,0 +1,23 @@
+# Python 3.10.4
+
+# Домашнее задание:
+# Задача с двумя друзьями и собакой – попробовать дома написать код, разбор будет на семинаре.
+
+count = 0
+distance = 10000
+
+first_friend_speed = 1
+second_friend_speed = 2
+dog_speed = 5
+
+friend = 2
+while distance > 10:
+    if friend == 1:
+        time = distance / (first_friend_speed + dog_speed)
+        friend = 2
+    else:
+        time = distance / (second_friend_speed + dog_speed)
+        friend = 1
+    distance = distance - (first_friend_speed + second_friend_speed) * time
+    count += 1
+print(count)
