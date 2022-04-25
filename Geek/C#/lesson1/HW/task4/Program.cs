ac7df9c213a25abc4454fdadf137db5db50b65b7
Greg <xyi7i@yandex.ru>
2022-04-25 13:10:41 +0300

    GeekBrains

--- Geek/C#/lesson1/HW/task4/Program.cs
+++ Geek/C#/lesson1/HW/task4/Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Задача 4. Программу на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.");
 
+Console.Write("Введите N: ");
+
+int N = int.Parse(Console.ReadLine ());
+
+int i = 1;
+
+ while (i <= N)
+ {
+    if (i % 2 == 0)
+    {
+             Console.Write(i + " ");
+    }
+
+     i++;
+ }
