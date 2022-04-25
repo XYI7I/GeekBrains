cce4c92b0262ae837af087bfe564d5d66e676c35
Greg <xyi7i@yandex.ru>
2022-04-25 13:35:04 +0300

    GeekBrains

---
+++ Geek/C#/lesson1/HW/task5/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Задача 5*. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.");
+int num = 1000;
+while (num > 999 ^ num < 100)
+{
+    Console.Write("Введите трехзначное число: ");
+    num  = int.Parse(Console.ReadLine ());
+    string text = num.ToString(); 
+    text = text.Substring(2);
+    Console.WriteLine(text);
+}
