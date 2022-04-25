51b8f6d6974048958414fa42bc7ff24194ebb635
Greg <xyi7i@yandex.ru>
2022-04-25 12:52:49 +0300

    GeekBrains

---
+++ Geek/C#/lesson1/HW/task3/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Задача 3. Программа на вход принимает число и выдаёт, является ли число чётным.");
---
+++ Geek/C#/lesson1/HW/task4/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Задача 4. Программу на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.");
+
