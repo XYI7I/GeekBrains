89f7b8b2335e0ab350b6d527fe268ec44271d84c
Greg <xyi7i@yandex.ru>
2022-05-03 14:42:23 +0300

    Lesson4

--- AI/Python/Lesson4/module.py
+++ AI/Python/Lesson4/module.py
@@ -1,6 +1,11 @@
 from random import randint
 
 def gen_randint_list(N):
+    '''
+    Generator of random integer list.
+    :param N: - element's quantity
+    :return: - random integer list
+    '''
     new_list = []
 
     for el in range(N):
@@ -10,6 +15,11 @@ def gen_randint_list(N):
     return new_list
 
 def compare(list):
+    '''
+    Compare neighboring list's elements if element[i] > element[i-1]
+    :param list: element's list
+    :return: new list with elements from list which element[i] > element[i-1]
+    '''
     new_list = []
     for el in range(1, len(list)):
         if list[el] > list[el - 1]:
