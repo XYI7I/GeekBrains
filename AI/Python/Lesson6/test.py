3207ad0e83622e475b1c31abc7255cc6e4735c0f
Greg <xyi7i@yandex.ru>
2022-05-06 16:52:29 +0300

    Lesson7

--- AI/Python/Lesson6/test.py
+++
@@ -1,8 +0,0 @@
-class User:
-    def __str__(self):
-        return 'User'
-
-
-my_user = User()
-
-print(my_user)
---
+++ AI/Python/Lesson7/test.py
@@ -0,0 +1,44 @@
+class User(object):
+
+    def __init__(self, login, email=None):
+        self.username = login       # public
+        self.__password = 'abc'     # private
+        self.email = email
+
+    # def __show_password(self):      # private method or
+    def show_password(self):      # public method
+        print(self.__password)
+
+    def set_email(self, new_email):
+        self.email = new_email
+
+    def __str__(self):
+        return f'User - {self.username}'
+
+
+class Admin(User):
+
+    def set_email(self, new_email):
+        super().set_email(new_email=new_email)
+        print('e-mail')
+
+    def del_photo(self):
+        print('Delete photo')
+
+    def __str__(self):
+        return f'Root - {self.username}'
+
+
+my_user = User(login='admin', email='admin@mail.ru')
+other_user = User(login='root', email='root@mail.ru')
+admin = Admin(login='administrator')
+
+print(my_user.username)
+print(my_user.email)
+print(my_user.set_email('admin@google.com'))
+print(my_user.email)
+
+print(other_user)
+print(other_user.email)
+other_user.show_password()
+admin.del_photo()
