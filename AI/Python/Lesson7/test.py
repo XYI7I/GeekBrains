186d2b01931a209a1f8f715f4148b9d44002fcdd
Greg <xyi7i@yandex.ru>
2022-05-08 20:18:51 +0300

    Lesson7

--- AI/Python/Lesson7/test.py
+++ AI/Python/Lesson7/test.py
@@ -1,12 +1,12 @@
 class User(object):
 
     def __init__(self, login, email=None):
-        self.username = login       # public
-        self.__password = 'abc'     # private
+        self.username = login  # public
+        self.__password = 'abc'  # private
         self.email = email
 
     # def __show_password(self):      # private method or
-    def show_password(self):      # public method
+    def show_password(self):  # public method
         print(self.__password)
 
     def set_email(self, new_email):
@@ -28,6 +28,26 @@ class Admin(User):
     def __str__(self):
         return f'Root - {self.username}'
 
+
+class Number:
+
+    def __init__(self, number):
+        self.value = number
+
+    def __add__(self, other):
+        self.value += other
+        return self.value
+
+    def __sub__(self, other):
+        self.value -= other
+        return self.value
+
+    def __str__(self):
+        return str(self.value)
+
+
+my_num = Number(5)
+print(my_num + 3)
 
 my_user = User(login='admin', email='admin@mail.ru')
 other_user = User(login='root', email='root@mail.ru')
