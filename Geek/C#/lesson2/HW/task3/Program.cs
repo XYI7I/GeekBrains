22ebf47ef89270074c0c085985b516e56435baaa
XYI7I <xyi7i@yandex.ru>
2022-05-14 13:14:41 +0300

    Lesson2

---
+++ Geek/C#/lesson2/HW/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+
+Console.Write("Введите число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+int absnum = Math.Abs(num);
+string text = absnum.ToString();
+string res = text.Length < 3 ? $"{num} - третьей цифры нет" : $"{num} -> {text[2]}";
+
+Console.WriteLine(res);
