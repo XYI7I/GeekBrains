3fbfceca20a9b248b7419790d988ad0c33980dcb
Greg <xyi7i@yandex.ru>
2022-05-15 11:40:50 +0300

    Lesson3

--- Geek/C#/lesson3/task5/Program.cs
+++ Geek/C#/lesson3/task5/Program.cs
@@ -4,18 +4,6 @@ Console.WriteLine("Программа, принимает на вход числ
 Console.Write("Ведите число N: ");
 int Num = int.Parse(Console.ReadLine ());
 
-int[] arr = new int[Num];
-
-for (int i = 0; i < Num; i++)
-{
-    arr [i] = (i + 1) * (i + 1);
-}
-
-var str = string.Join(" ", arr);
-Console.WriteLine($"{Num} -> {str}");
-
-
-
 void SquareArray(int Num)
 {
     int[] array = new int[Num];
