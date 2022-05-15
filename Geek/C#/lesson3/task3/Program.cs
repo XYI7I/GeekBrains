e3103da6d25b5b227483d622cda1d2ded9181bed
Greg <xyi7i@yandex.ru>
2022-05-15 11:02:51 +0300

    Lesson3

--- Geek/C#/lesson3/task3/Program.cs
+++ Geek/C#/lesson3/task3/Program.cs
@@ -1,26 +1,31 @@
 ﻿// 18. Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
 Console.WriteLine("Программа, по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).");
 
-int num_Q = 0;
-string res = null;
-// string inf = "∞";
-
-
-Console.WriteLine(inf);
-
-while (num_Q <= 0 || num_Q > 4)
+int Input(int num_Q)
 {
-    Console.Write("Ведите номер четверти, Q [1, 4]: ");
-    num_Q = int.Parse(Console.ReadLine ());
-}
-if (num_Q == 1 || num_Q == 4)
-{
-    res = num_Q == 1? $"X [0, +inf] Y [0, +inf]" : $"X [0, +inf] Y [0, -inf]";
+    while (num_Q <= 0 || num_Q > 4)
+    {
+        Console.Write("Ведите номер четверти, Q [1, 4]: ");
+        num_Q = int.Parse(Console.ReadLine ());
+    }
+    return num_Q;
 }
 
-else
+void Res_Q(int num_Q)
 {
-    res = num_Q == 2? $"X [0, -inf] Y [0, +inf]" : $"X [0, -inf] Y [0, -inf]";
+    string res = null;
+    if (num_Q == 1 || num_Q == 4)
+    {
+        res = num_Q == 1? $"X > 0, Y > 0" : $"X > 0, Y < 0";
+    }
+
+    else
+    {
+        res = num_Q == 2? $"X < 0, Y > 0" : $"X < 0, Y < 0";
+    }
+
+    Console.WriteLine(res);
 }
 
-Console.WriteLine(res);
+int Q = Input(0);
+Res_Q(Q);
