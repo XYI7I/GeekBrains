0797affd48dbe990fc71abcd08ad45bdea37be42
XYI7I <xyi7i@yandex.ru>
2022-05-21 16:59:30 +0300

    Lesson4

--- Geek/C#/lesson4/HW/task4/Program.cs
+++ Geek/C#/lesson4/HW/task4/Program.cs
@@ -1,20 +1,22 @@
 ﻿// **. Напишите функцию, которая принимает слово и возвращает true, если в этом слове есть две одинаковые, стоящие рядом буквы.
 Console.WriteLine("Функция возвращает true, если в слове есть две одинаковые, стоящие рядом буквы.");
 
-void CharDoubl(string word)
+bool CharDoubl(string word)
 {
-    bool check = false;
     for (int i = 0; i < word.Length - 1; i++)
     {
         if (word[i] == word[i + 1])
         {
-            check = true;
+            return true;
             break;
             //i = word.Length;
         }
     }
-
-    Console.WriteLine(check);
+    return false;
 }
 
-CharDoubl("12354");
+// Console.Write("Введите слово: ");
+// string wordtest = Console.ReadLine ();
+// bool che = CharDoubl(wordtest);
+// Console.WriteLine(CharDoubl(wordtest));
+// Console.WriteLine(CharDoubl("112348sfergg"));
