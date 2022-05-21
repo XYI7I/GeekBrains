3396bc892c0a869cb4c8c6afb1f455e994294764
XYI7I <xyi7i@yandex.ru>
2022-05-21 11:29:41 +0300

    Lesson4

---
+++ Geek/C#/lesson4/task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа по генерации одномерного массива:
+Console.WriteLine("Программа по генерации одномерного одномерного массива");
+
+int[] arr = new int[8];
+Random rand = new Random();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(2);
+    //Console.WriteLine(arr[i]);
+}
+
+var str = string.Join(" ", arr);
+Console.WriteLine(str); 
+
+
+
