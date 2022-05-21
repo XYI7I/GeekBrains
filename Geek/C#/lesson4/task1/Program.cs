f650f823d138fa98e69df70a696f416e8f96840b
XYI7I <xyi7i@yandex.ru>
2022-05-21 10:51:52 +0300

    Lesson4

--- Geek/C#/lesson4/task1/Program.cs
+++ Geek/C#/lesson4/task1/Program.cs
@@ -1,3 +1,26 @@
-﻿// Программа по сортировке одномерного массива:
-Console.WriteLine("Программа по сортировке одномерного массива");
-;
+﻿// 22. Напишите программу, которая принимает на вход число (A)и выдает сумму всех чисел от 1 до A.
+Console.WriteLine("Программу, которая принимает на вход число (A)и выдает сумму всех чисел от 1 до A.");
+
+Console.Write("Введите A: ");
+
+int a = int.Parse(Console.ReadLine ());
+
+int sum = 0;
+
+int i = 1;
+
+ while (i <= a)
+ {
+     sum = sum + i;
+     i++; 
+ }
+
+ Console.WriteLine(a + " -> " + sum);
+
+int sum1 = 0;
+for (int j = 1; j <= a; j++)
+{
+    sum1 = sum1 + j;
+}
+
+Console.WriteLine(a + " -> " + sum1);  
---
+++ Geek/C#/lesson4/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// 26. Напишите программу, которая принимает на вход число и выдает количество цифр в числе.
+Console.WriteLine("Программу, которая принимает на вход число и выдает количество цифр в числе.");
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine ());
+
+int dev = 10;
+int dig_w = 1;
+
+while (num / dev != 0)
+{
+    dev = dev * 10;
+    dig_w = dig_w + 1;
+}
+Console.WriteLine($"Количество цифр в числе {num} -> {dig_w}");
+
+int dig = 0;
+for (int i = 1; i < num; i = i * 10)
+{
+    dig++;
+}
+
+Console.WriteLine($"Количество цифр в числе {num} -> {dig}");
