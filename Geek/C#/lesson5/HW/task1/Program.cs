a6bc9d6ee1eafcf5a8c53cd292f94657c593c733
Greg <xyi7i@yandex.ru>
2022-05-22 18:12:56 +0300

    Lesson5

---
+++ Geek/C#/lesson5/HW/task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// 34. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.WriteLine("Программа показывает количество чётных чисел в массиве.");
+
+int[] GenArray()
+{
+    Console.Write("Введите число элементов массива: ");
+    int n = int.Parse(Console.ReadLine ());
+    int[] arr = new int[n];
+    Random rand = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(100, 1000);
+    }
+    return arr;
+}
+
+void PrintArray(int[] prarr)
+{
+    var str = string.Join(" ", prarr);
+    Console.WriteLine(str);
+}
+
+int FindEvenNumArray(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] newarr = GenArray();
+PrintArray(newarr);
+int counint = FindEvenNumArray(newarr);
+Console.WriteLine($"Количество четных чисел в массиве -> {counint}");
