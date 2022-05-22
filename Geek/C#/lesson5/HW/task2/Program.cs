407562b28a19555533984208a834d65a9f5f5888
Greg <xyi7i@yandex.ru>
2022-05-22 18:19:00 +0300

    Lesson5

--- Geek/C#/lesson5/HW/task2/Program.cs
+++ Geek/C#/lesson5/HW/task2/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 36.  Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+Console.WriteLine("Программа показывает сумму элементов в масиве стоящщих на четных местах.");
+
+int[] GenArray()
+{
+    Console.Write("Введите число элементов массива: ");
+    int n = int.Parse(Console.ReadLine ());
+    int[] arr = new int[n];
+    Random rand = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(-100, 101);
+    }
+    return arr;
+}
+
+void PrintArray(int[] prarr)
+{
+    var str = string.Join(" ", prarr);
+    Console.WriteLine(str);
+}
+
+int SumEvenIndArray(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i = i + 2)
+    {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+int[] newarr = GenArray();
+PrintArray(newarr);
+int counint = SumEvenIndArray(newarr);
+Console.WriteLine($"Сумма элементов в масиве стоящщих на четных местах -> {counint}");
