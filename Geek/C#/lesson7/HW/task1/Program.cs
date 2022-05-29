82ba7040b76aa989b366ab68f43c7ca4ac20d63d
Greg <xyi7i@yandex.ru>
2022-05-29 11:59:18 +0300

    Lesson7

---
+++ Geek/C#/lesson7/HW/task1/Program.cs
@@ -0,0 +1,37 @@
+﻿// 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.WriteLine("Программа создает двумерный массив размером m×n, заполненный случайными вещественными числами.");
+
+int[,] GenArray()
+{
+    Console.Write("Введите число строк массива m: ");
+    int m = int.Parse(Console.ReadLine ());
+    Console.Write("Введите число строк массива n: ");
+    int n = int.Parse(Console.ReadLine ());
+    int[,] arr = new int[m, n];
+    Random rand = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = rand.Next(-10, 11);
+        }
+
+    }
+    return arr;
+}
+
+void PrintArray(int[,] prarr)
+{
+    for (int i = 0; i < prarr.GetLength(0); i++)
+    {
+        for (int j = 0; j < prarr.GetLength(1); j++)
+        {
+            Console.Write(prarr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = GenArray();
+PrintArray(array);
