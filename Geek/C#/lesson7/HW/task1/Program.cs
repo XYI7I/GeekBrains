35d370b298b37cc3cbc20af890e7fb288551aaa3
Greg <xyi7i@yandex.ru>
2022-05-29 15:43:32 +0300

    Lesson7

--- Geek/C#/lesson7/HW/task1/Program.cs
+++ Geek/C#/lesson7/HW/task1/Program.cs
@@ -1,37 +1,37 @@
 ﻿// 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 Console.WriteLine("Программа создает двумерный массив размером m×n, заполненный случайными вещественными числами.");
 
-int[,] GenArray()
+double [,] GenDoubleArray()
 {
     Console.Write("Введите число строк массива m: ");
     int m = int.Parse(Console.ReadLine ());
     Console.Write("Введите число строк массива n: ");
     int n = int.Parse(Console.ReadLine ());
-    int[,] arr = new int[m, n];
+    double[,] arr = new double[m, n];
     Random rand = new Random();
 
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            arr[i, j] = rand.Next(-10, 11);
+            arr[i, j] = Math.Round(rand.Next(-10, 11) * rand.NextDouble(), 1);
         }
 
     }
     return arr;
 }
 
-void PrintArray(int[,] prarr)
+void PrintDoubleArray(double[,] prarr)
 {
     for (int i = 0; i < prarr.GetLength(0); i++)
     {
         for (int j = 0; j < prarr.GetLength(1); j++)
         {
-            Console.Write(prarr[i,j] + " ");
+            Console.Write(prarr[i,j] + "\t");
         }
         Console.WriteLine();
     }
 }
 
-int[,] array = GenArray();
-PrintArray(array);
+double[,] array = GenDoubleArray();
+PrintDoubleArray(array);
