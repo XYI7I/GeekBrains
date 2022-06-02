5d65e7ddfb0d5dc5bf905ae734f6e5feb2e9fe28
Greg <xyi7i@yandex.ru>
2022-06-02 14:58:12 +0300

    Lesson5

--- Geek/C#/lesson5/HW/task2/Program.cs
+++ Geek/C#/lesson5/HW/task2/Program.cs
@@ -15,7 +15,28 @@ int[] GenArray()
     return arr;
 }
 
+double [] GenDoubleArray()
+{
+    Console.Write("Введите число элементов массива: ");
+    int n = int.Parse(Console.ReadLine ());
+    double[] arr = new double[n];
+    Random rand = new Random( );
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Math.Round(rand.Next(-100, 101) * rand.NextDouble(), 1);
+    }
+    return arr;
+}
+
 void PrintArray(int[] prarr)
+{
+    var str = string.Join(" ", prarr);
+    Console.WriteLine(str);
+}
+
+
+void PrintDoubleArray(double[] prarr)
 {
     var str = string.Join(" ", prarr);
     Console.WriteLine(str);
@@ -24,7 +45,16 @@ void PrintArray(int[] prarr)
 int SumEvenIndArray(int[] arr)
 {
     int sum = 0;
-    for (int i = 0; i < arr.Length; i = i + 2)
+    for (int i = 1; i < arr.Length; i = i + 2)
+    {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+double SumEvenIndDoubleArray(double[] arr)
+{
+    double sum = 0;
+    for (int i = 1; i < arr.Length; i = i + 2)
     {
         sum = sum + arr[i];
     }
@@ -36,3 +66,9 @@ PrintArray(newarr);
 
 int sumevin = SumEvenIndArray(newarr);
 Console.WriteLine($"Сумма элементов в масиве стоящщих на четных местах -> {sumevin}");
+
+double[] newarr1 = GenDoubleArray();
+PrintDoubleArray(newarr1);
+
+double sumevin1 = SumEvenIndDoubleArray(newarr1);
+Console.WriteLine($"Сумма элементов в масиве стоящщих на четных местах -> {sumevin1}");
