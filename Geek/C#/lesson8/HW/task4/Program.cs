05480c7150d5bb4e49a947785fe875e7452b3513
Greg <xyi7i@yandex.ru>
2022-06-04 15:56:45 +0300

    Lesson8

---
+++ Geek/C#/lesson8/HW/task4/Program.cs
@@ -0,0 +1,34 @@
+﻿// 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Console.WriteLine("Программа будет построчно выводить трехмерный массив, добавляя индексы каждого элемента.");
+
+int[,] GenArray()
+{
+    Console.Write("Введите число строк массива m: ");
+    int m = int.Parse(Console.ReadLine ());
+    Console.Write("Введите число столбцов массива n: ");
+    int n = int.Parse(Console.ReadLine ());
+    int[,] arr = new int[m, n];
+    Random rand = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = rand.Next(10);
+        }
+
+    }
+    return arr;
+}
+
+void PrintArray(int[,] prarr)
+{
+    for (int i = 0; i < prarr.GetLength(0); i++)
+    {
+        for (int j = 0; j < prarr.GetLength(1); j++)
+        {
+            Console.Write(prarr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Geek/C#/lesson8/HW/task5/Program.cs
@@ -0,0 +1,30 @@
+﻿// 62. Заполните спирально массив 4 на 4.
+Console.WriteLine("Программа заполнит спиралыьно массив 4 на 4.");
+
+int[,] SpiralArray4x4()
+{
+    int[,] arr = new int[4, 4];
+  
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = k++;
+        }
+
+    }
+    return arr;
+}
+
+void PrintArray(int[,] prarr)
+{
+    for (int i = 0; i < prarr.GetLength(0); i++)
+    {
+        for (int j = 0; j < prarr.GetLength(1); j++)
+        {
+            Console.Write(prarr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
