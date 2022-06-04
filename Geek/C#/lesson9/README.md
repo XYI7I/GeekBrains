dfb9bffbcaec450d1ef381998166654fb883f645
Greg <xyi7i@yandex.ru>
2022-06-04 22:30:57 +0300

    Lesson9

---
+++ Geek/C#/lesson9/README.md
@@ -0,0 +1,16 @@
+﻿# [![GeekBrains](https://frontend-scripts.hb.bizmrg.com/unique-hf/svg/logo.svg)](https://gb.ru)
+
+## Знакомство с языками программирования (семинары)
+
+### Урок 9. Как не нужно писать код. Часть 3
+
+> [Задача ](https://github.com/XYI7I/GeekBrains/blob/main/Geek/C%23/lesson9/task1/Program.cs): 
+
+
+> [Задача ](https://github.com/XYI7I/GeekBrains/tree/main/Geek/C%23/lesson9/task2/Program.cs): 
+
+
+> [Задача ](https://github.com/XYI7I/GeekBrains/tree/main/Geek/C%23/lesson9/task3/Program.cs): 
+
+        
+> [Задача ](https://github.com/XYI7I/GeekBrains/tree/main/Geek/C%23/lesson9/task4/Program.cs):
---
+++ Geek/C#/lesson9/task2/Program.cs
@@ -0,0 +1,34 @@
+﻿// . 
+Console.WriteLine("Программа ");
+
+int[,] GenArray()
+{
+    Console.Write("Введите число строк массива m: ");
+    int m = int.Parse(Console.ReadLine ());
+    Console.Write("Введите число столбцов массива n: ");
+    int n = int.Parse(Console.ReadLine ());
+    int[,] arr = new int[m, n];
+    Random rand = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = rand.Next(10);
+        }
+
+    }
+    return arr;
+}
+
+void PrintArray(int[,] prarr)
+{
+    for (int i = 0; i < prarr.GetLength(0); i++)
+    {
+        for (int j = 0; j < prarr.GetLength(1); j++)
+        {
+            Console.Write(prarr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
