42f56c071009173aa4fed501a1e829cbd5e77946
Greg <xyi7i@yandex.ru>
2022-06-04 22:22:50 +0300

    Lesson8

--- Geek/C#/lesson8/HW/task5/Program.cs
+++ Geek/C#/lesson8/HW/task5/Program.cs
@@ -1,17 +1,32 @@
 ﻿// 62. Заполните спирально массив 4 на 4.
-Console.WriteLine("Программа заполнит спиралыьно массив 4 на 4.");
+Console.WriteLine("Программа заполнит спиралыьно массив N x N.");
 
-int[,] SpiralArray4x4()
+int[,] SpiralArrayNxN()
 {
-    int[,] arr = new int[4, 4];
-    int k = 1;  
+    Console.Write("Введите размер массива NxN, N: ");
+    int n = int.Parse(Console.ReadLine ());
+    int[,] arr = new int[n, n];
+    int k = 1;
+    int itr = n/2 + n%2;
 
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < itr; i++)
     {
-        for (int j = 0; j < 4; j++)
+        for (int j = i; j < n - i; j++)
         {
-            arr[i, j] = k++;
+            arr[i,j] = k++;
+            if (k == n * n)
+                return arr;
+        }            
+        for (int j = i + 1; j < n-i; j++)
+            arr[j,n - i - 1] = k++;
+        for (int j = n - i - 2; j >= i; j--)
+        {
+            arr[n - i - 1,j] = k++;
+            if (k == n * n)
+                return arr;
         }
+        for (int j = n - i - 2; j >= i + 1; j--)
+            arr[j,i] = k++;
 
     }
     return arr;
@@ -23,29 +38,11 @@ void PrintArray(int[,] prarr)
     {
         for (int j = 0; j < prarr.GetLength(1); j++)
         {
-            Console.Write(prarr[i,j] + " ");
+            Console.Write(prarr[i,j] + "\t");
         }
         Console.WriteLine();
     }
 }
 
-int[,] arr = SpiralArray4x4();
+int[,] arr = SpiralArrayNxN();
 PrintArray(arr);
-
-
-    0.0
-    0.1
-    0.2
-    0.3
-    1.3
-    2.3
-    3.3
-    3.2
-    3.1
-    3.0
-    2.0
-    1.0
-    1.1
-    1.2
-    2.2
-    2.1
