cc81b59fc82ff9db9b3ffa0ce3e4c98c1539e7f3
Greg Koynash <71480610+XYI7I@users.noreply.github.com>
2022-06-11 14:58:31 +0300

    Lesson10

---
+++ Geek/C#/lesson10/task1/Program.cs
@@ -0,0 +1,34 @@
+﻿// 70. Напишите программу, которая на вход принимает два числа и выдаёт первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+Console.WriteLine("Программа выведет первые N чисел, для которых каждое следующее равно сумме двух предыдущих.");
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine ());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine ());
+
+Console.Write("Введите количество чисел N: ");
+int n = int.Parse(Console.ReadLine ());
+
+if (b < a)
+{
+    int temp = b;
+    b = a;
+    a = temp;
+}
+
+void PrintSumNumInt(int a, int b, int n)
+{
+    if(n == 0) return; 
+    Console.Write($"{a} ");
+    int temp = b;
+    b = a + b;
+    a = temp;
+    n--;
+    PrintSumNumInt(a, b, n);
+    return;
+}
+
+
+PrintSumNumInt(a, b, n);
+Console.WriteLine();
---
+++ Geek/C#/lesson10/task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// 71. В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита.
+Console.WriteLine("Программа покажет все слова, состоящие из n букв, которые можно построить из букв этого алфавита.");
+
+string charArray = "012345789";
+
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine ());
+
+void PrintAllWord(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        PrintAllWord(alphabet, word, length + 1);
+    }
+}
+
+PrintAllWord(charArray, new char[n]);
