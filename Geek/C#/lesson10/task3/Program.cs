719ea9ff74a49cd7bbdd4259804cab699cae3ff4
Greg <xyi7i@yandex.ru>
2022-06-13 20:30:26 +0300

    Lesson10

--- Geek/C#/lesson10/task3/Program.cs
+++ Geek/C#/lesson10/task3/Program.cs
@@ -1,18 +1,29 @@
 ﻿// 72. Заданы 2 массива: info и data. В массиве info хранятся двоичные представления нескольких чисел (без разделителя). В массиве data хранится информация о количестве бит, которые занимают числа из массива info. Напишите программу, которая составит массив десятичных представлений чисел массива data с учётом информации из массива info. 
-Console.WriteLine("Программа покажет все слова, состоящие из n букв, которые можно построить из букв этого алфавита.");
+Console.WriteLine("Программа составит массив десятичных представлений чисел массива data с учётом информации из массива info.");
 
 int[] data = {0, 1, 1, 1, 1, 0, 0, 0, 1};
 int[] info = {2, 3, 3, 1};
-int[] resultArr = new int[info.Length];
-int n = 0;
 
-for (int i = 0; i < info.Length; i++)
+void PrintArray(int[] prarr)
 {
-    for (int j = 0; j < info[i]; j++)
-    {   
-        if (data[n] == 0) resultArr[i] = 1;
-        resultArr[i] = 2 *  + r;
-        Console.WriteLine(resultArr[i]);
-    }
-    n = n + info[i];  
+    var str = string.Join(" ", prarr);
+    Console.WriteLine(str);
 }
+
+
+int[] BinArrToDecArr(int[] binarr, int[] info)
+{
+    int[] resultArr = new int[info.Length];
+    int n = 0;
+    for (int i = 0; i < info.Length; i++)
+    {
+        for (int j = 0; j < info[i]; j++)
+        {   
+            resultArr[i] += Convert.ToInt32(Math.Pow(2,info[i] - j - 1)) * binarr[n++];        
+        } 
+    }
+    return resultArr;
+}
+
+int[] resultArr = BinArrToDecArr(data, info);
+PrintArray(resultArr);
