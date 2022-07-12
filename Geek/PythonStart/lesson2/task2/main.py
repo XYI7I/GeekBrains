d95954f42e9bd9e25b329351de82b975633c6cb5
Greg <xyi7i@yandex.ru>
2022-07-12 21:03:00 +0300

    task2

--- Geek/PythonStart/lesson2/task2/main.py
+++ Geek/PythonStart/lesson2/task2/main.py
@@ -5,3 +5,21 @@
 
 - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 """
+
+def listFactN():
+    N = int(input('Input N: '))
+    listFactN = []
+    for i in range(N):
+        listFactN.append(findFact(i + 1))
+
+    print(listFactN)
+
+def findFact(n):
+    """Поиск факториала"""
+    facn = 1
+    for i in range(1, n):
+        facn = facn * i
+
+    return facn
+
+listFactN()
