bb2218c0e6f00740e1a746058fb6241e73ceaea4
Greg <xyi7i@yandex.ru>
2022-07-12 20:19:55 +0300

    task6

--- Geek/PythonStart/lesson1/task6/main.py
+++ Geek/PythonStart/lesson1/task6/main.py
@@ -7,3 +7,39 @@
 
 - найти наибольший простой делитель числа
 """
+
+
+def findFact():
+    """Поиск факториала"""
+    n = -1
+    while n < 0:
+        n = int(input('введите n (n ≥ 0): '))
+    facn = 1
+
+    for i in range(1, n):
+        facn = facn * 1
+
+    print(f'Факториал числа {n} - {n}! = {facn}')
+
+def findSimplNum():
+    """Определение простое число или нет"""
+    num = int(input('введите число: '))
+    for i in range(2, num):
+        if num % i == 0:
+            print(f'Число {num} - не простое')
+            return
+    print(f'Число {num} - простое')
+
+def findLargDiv():
+    """Определение простое число или нет"""
+    num = int(input('введите число: '))
+    maxdiv = 1
+    for i in range(2, num):
+        if num % i == 0:
+            maxdiv = i
+
+    print(f'Наибольший делитель числа {num} = {maxdiv}')
+
+findFact()
+findSimplNum()
+findLargDiv()
