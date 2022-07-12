1192cf464872bd370f110d6b51150b0e551f894c
Greg <xyi7i@yandex.ru>
2022-07-12 19:40:49 +0300

    task4

--- Geek/PythonStart/lesson1/task4/main.py
+++ Geek/PythonStart/lesson1/task4/main.py
@@ -1,16 +1,20 @@
 """
 Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
 """
+def findRangeXY(numQ):
+    result_str = 'X > 0 и Y < 0'
+    if numQ == 1:
+        result_str = 'X > 0 и Y > 0'
+    elif numQ == 2:
+        result_str = 'X < 0 и Y > 0'
+    elif numQ == 3:
+        result_str = 'X < 0 и Y < 0'
+
+    print(f"Диапазон возможных координат находится в следющих интервалах: {result_str}")
+
 numQ = 0
 
 while numQ < 1 or numQ > 4:
     numQ = int(input('Введите номер четверти Q (1-4): '))
 
-if numQ == 1:
-    print('X > 0; Y > 0')
-elif numQ == 2:
-    print('X < 0; Y > 0')
-elif numQ == 3:
-    print('X < 0; Y < 0')
-else:
-    print('X > 0; Y < 0')
+findRangeXY(numQ)
