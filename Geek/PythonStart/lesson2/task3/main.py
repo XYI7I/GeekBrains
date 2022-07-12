e8e182f10bb9c3d3b7236536f8857b2c36db9f87
Greg <xyi7i@yandex.ru>
2022-07-12 22:14:47 +0300

    task3

--- Geek/PythonStart/lesson2/task3/main.py
+++ Geek/PythonStart/lesson2/task3/main.py
@@ -5,3 +5,14 @@
 
 - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 """
+def makeListSubseq(N):
+    listNum =[]
+    sum = 0
+    for i in range(1, N + 1):
+        num = (1 + 1 / i) ** i
+        sum += num
+        listNum.append(round(num, 4))
+    return listNum, sum
+
+listSubseq, SubseqSum = makeListSubseq(3)
+print(f'Сумма элементов последовательности {listSubseq} = {round(SubseqSum, 4)}')
