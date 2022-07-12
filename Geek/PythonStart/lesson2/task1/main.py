844eaeb3d20b257b540f788c55d511eb67f7777a
Greg <xyi7i@yandex.ru>
2022-07-12 20:51:22 +0300

    task1

--- Geek/PythonStart/lesson2/task1/main.py
+++ Geek/PythonStart/lesson2/task1/main.py
@@ -7,3 +7,15 @@
 - 0,56 -> 11
 
 """
+def findSumDigofNum():
+    num = input('Input number: ')
+    SumDig = 0
+    for el in range(len(num)):
+        try:
+            SumDig += int(num[el])
+        except:
+            pass
+    return num, SumDig
+num, sumdig = findSumDigofNum()
+
+print(f'Сумма цифр в числе {num} = {sumdig}')
