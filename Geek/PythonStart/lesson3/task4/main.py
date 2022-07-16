25c386670438c82381abed87426c6267b8c8af0f
XYI7I <xyi7i@yandex.ru>
2022-07-16 13:00:55 +0300

    task4

--- Geek/PythonStart/lesson3/task4/main.py
+++ Geek/PythonStart/lesson3/task4/main.py
@@ -6,3 +6,27 @@
 - 3 -> 11
 - 2 -> 10
  """
+
+
+def dec_to_bin(num):
+    """
+    Функция преобразовывает десятичное число в двоичное.
+
+    Пример:
+    - 45 -> 101101
+
+    :param num: int
+    """
+    num_bin = ''
+    if num == 0:
+        num_bin = '0'
+    while num != 0:
+        num_bin = str(num % 2) + num_bin
+        num //= 2
+
+    print(num_bin)
+
+
+dec_to_bin(45)
+dec_to_bin(3)
+dec_to_bin(2)
