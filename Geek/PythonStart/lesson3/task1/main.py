878722b840679a853a06044d23aa13f6b0c049dc
XYI7I <xyi7i@yandex.ru>
2022-07-16 11:57:50 +0300

    task1

--- Geek/PythonStart/lesson3/task1/main.py
+++ Geek/PythonStart/lesson3/task1/main.py
@@ -7,4 +7,20 @@
 
 """
 
+list_num = [2, 3, 5, 9, 3]
 
+
+def find_sum_el_odd_pos(list_num):
+    """
+    Функция находит сумму элементов на нечетных позициях
+
+    :param list_num: - список из чисел
+    """
+    sum_el = 0
+    for i in range(1, len(list_num), 2):
+        sum_el += list_num[i]
+    return sum_el
+
+
+find_sum = find_sum_el_odd_pos(list_num)
+print(find_sum)
