ae422315e0fd0189991d1529b6977aee811ce47b
XYI7I <xyi7i@yandex.ru>
2022-07-16 12:21:14 +0300

    task2

--- Geek/PythonStart/lesson3/task2/main.py
+++ Geek/PythonStart/lesson3/task2/main.py
@@ -7,5 +7,28 @@
 - [2, 3, 5, 6] => [12, 15]
 """
 
+list1 = [2, 3, 4, 5, 6]
+list2 = [2, 3, 5, 6]
+
+def find_mul_pair_pos(list_num):
+    """
+    Функция находит произведение элементов произведение пар чисел списка
+
+    Пример:
+    - [2, 3, 4, 5, 6] => [12, 15, 16];
+    - [2, 3, 5, 6] => [12, 15]
+    """
+    mul_list = []
+    for i in range(len(list_num)//2 + len(list_num) % 2):
+        mul_list.append(list_num[i] * list_num[len(list_num) - i - 1])
+    return mul_list
+
+
+find_mult1 = find_mul_pair_pos(list1)
+find_mult2 = find_mul_pair_pos(list2)
+
+print(f' Произведение пар чисел списка {list1} = {find_mult1}')
+print(f' Произведение пар чисел списка {list2} = {find_mult2}')
+
 
 
