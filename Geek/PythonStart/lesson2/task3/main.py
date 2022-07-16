1d3c7754afc99064495724c1fc18b89f39e3fc99
XYI7I <xyi7i@yandex.ru>
2022-07-16 10:44:05 +0300

    task3

--- Geek/PythonStart/lesson2/task3/main.py
+++ Geek/PythonStart/lesson2/task3/main.py
@@ -5,14 +5,20 @@
 
 - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 """
-def makeListSubseq(N):
-    listNum =[]
-    sum = 0
-    for i in range(1, N + 1):
-        num = (1 + 1 / i) ** i
-        sum += num
-        listNum.append(round(num, 4))
-    return listNum, sum
 
-listSubseq, SubseqSum = makeListSubseq(3)
+
+def make_list_subseq(n):
+    """
+    Функция создает список из n чисел последовательности (1+1/n)^n
+    """
+    listNum = []
+    sum_el = 0
+    for i in range(1, n + 1):
+        num = (1 + 1 / i) ** i
+        sum_el += num
+        listNum.append(round(num, 4))
+    return listNum, sum_el
+
+
+listSubseq, SubseqSum = make_list_subseq(3)
 print(f'Сумма элементов последовательности {listSubseq} = {round(SubseqSum, 4)}')
