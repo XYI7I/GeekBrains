780ae78dcd7f1bc2bc203f0d05078b076bad7a5b
XYI7I <xyi7i@yandex.ru>
2022-07-16 10:59:52 +0300

    task5

--- Geek/PythonStart/lesson2/task5/main.py
+++ Geek/PythonStart/lesson2/task5/main.py
@@ -8,24 +8,24 @@ def make_list_num_n_elem(n):
     """
     Функция создает список из n - случайных элементов промежутка [-N, N].
     """
-    listNum = []
+    list_num = []
     for i in range(n):
-        listNum.append(randint(-1 * n, n))
-    return listNum
+        list_num.append(randint(-1 * n, n))
+    return list_num
 
 
 def shuffle_list(list_el):
     """
     Функция перемешивает список.
     """
-    newlist = []
-    countlist = []
-    while len(countlist) != len(list_el):
+    new_list = []
+    count_list = []
+    while len(count_list) != len(list_el):
         pos = randint(0, len(list_el) - 1)
-        if pos not in countlist:
-            countlist.append(pos)
-            newlist.append(list_el[pos])
-    return newlist
+        if pos not in count_list:
+            count_list.append(pos)
+            new_list.append(list_el[pos])
+    return new_list
 
 
 list1 = make_list_num_n_elem(10)
