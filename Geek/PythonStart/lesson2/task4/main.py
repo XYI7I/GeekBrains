517deca1304b676756a18af98ae0f5811adfe391
XYI7I <xyi7i@yandex.ru>
2022-07-16 10:54:19 +0300

    task4

--- Geek/PythonStart/lesson2/task4/main.py
+++ Geek/PythonStart/lesson2/task4/main.py
@@ -3,22 +3,31 @@
  """
 from random import randint
 
-def makeListNumNelem(N):
-    listNum =[]
+
+def make_list_num_n_elem(N):
+    """
+    Функция создает список из n - случайных элементов промежутка [-N, N].
+    """
+    listNum = []
     for i in range(N):
         listNum.append(randint(-1 * N, N))
     return listNum
 
-def findMultListElPosition(list):
+
+def find_mult_list_el_position(list_n):
+    """
+    Функция находит произведение элементов списка находящихся на позициях заданых в текстовом файле.
+    """
     file = open('file.txt', 'r')
     Lines = file.readlines()
     mult = 1
     for line in Lines:
-        mult *= list[int(line)]
+        mult *= list_n[int(line)]
     return mult
 
-list1 = makeListNumNelem(10)
+
+list1 = make_list_num_n_elem(10)
 print(list1)
 
-multlistel = findMultListElPosition(list1)
-print(f'произведение элементов на указанных позициях (file.txt) = {multlistel}')
+mult_list_el = find_mult_list_el_position(list1)
+print(f'произведение элементов на указанных позициях (file.txt) = {mult_list_el}')
