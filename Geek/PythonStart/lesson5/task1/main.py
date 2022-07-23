fb2a2a52e4162b71855c069fb58477b86bc6e752
Greg <xyi7i@yandex.ru>
2022-07-23 11:00:10 +0300

    task1

--- Geek/PythonStart/lesson5/task1/main.py
+++ Geek/PythonStart/lesson5/task1/main.py
@@ -4,3 +4,18 @@
 
 """
 
+def find_lose_num(file_name):
+    """
+    Функция находит пропущеное число в списке условие A[i] - 1 = A[i-1].
+    """
+    file = open(file_name, 'r')
+    numbers = file.read().split()
+    for i in range(0, len(numbers) - 1) :
+        if int(numbers[i + 1]) - int(numbers[i]) != 1:
+            fnum = int(numbers[i]) + 1
+        i += 1
+    return fnum
+
+
+fnum = find_lose_num('number.txt')
+print(fnum)
