8d605a823ffb6f0c217130caf1860adf55425beb
Greg <xyi7i@yandex.ru>
2022-07-23 14:54:01 +0300

    task4

--- Geek/PythonStart/lesson4/task4/main.py
+++ Geek/PythonStart/lesson4/task4/main.py
@@ -4,4 +4,25 @@
 Пример:
 - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 """
-def
+
+def create_polyvalent_k(k):
+    """
+    Функция создает многочелен степени k и записывает его в фаил
+    значения коэффициентов случайны от 0 до 100)
+
+    Пример:
+    - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+    """
+    from random import randint
+    file = open('polyvalent', 'a+')
+    koeff_list = []
+    for i in range(0, k + 1):
+        koeff = str(randint(0, 100))
+        file.write(koeff + ' ')
+
+    file.write('\n')
+    file.close()
+
+
+
+create_polyvalent_k(2)
