0c4da149c4a8b908a711f83d0bba47981a35a3ad
Greg <xyi7i@yandex.ru>
2022-07-23 16:22:02 +0300

    task4

--- Geek/PythonStart/lesson4/task4/main.py
+++ Geek/PythonStart/lesson4/task4/main.py
@@ -23,6 +23,37 @@ def create_polyvalent_k(k):
     file.write('\n')
     file.close()
 
+def create_polyvalent_file_k(k):
+    """
+    Функция создает многочелен степени k и записывает его в фаил
+    значения коэффициентов случайны от 0 до 100)
+
+    Пример:
+    - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+    """
+    from random import randint
+    file = open('polyvalent_str', 'a+')
+    j = 0
+    for i in range(0, k + 1):
+        koeff = randint(0, 3)
+        print(koeff)
+        if koeff == 0:
+            j += 1
+            if j == k and koeff != 0:
+                return
+            else:
+                pass
+        elif j == i and (k - i) > 1:
+            file.write(str(koeff) + 'x^' + str(k - i))
+        elif j == i and (k - i) == 1:
+            file.write(str(koeff) + 'x')
+        elif (k - i) == 1:
+            file.write(' + ' + str(koeff) + 'x')
+        elif (k - i) == 0:
+            file.write(' + ' + str(koeff))
+
+    file.write(' = 0\n')
+    file.close()
+create_polyvalent_file_k(2)
 
 
-create_polyvalent_k(2)
