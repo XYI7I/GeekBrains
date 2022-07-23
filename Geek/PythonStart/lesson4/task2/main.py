a73dc7ed44e0c685571fee3eaf27e336a4597015
Greg <xyi7i@yandex.ru>
2022-07-23 13:06:36 +0300

    task2

--- Geek/PythonStart/lesson4/task2/main.py
+++ Geek/PythonStart/lesson4/task2/main.py
@@ -9,7 +9,7 @@ def find_list_simpl_div(n):
     Функция составит список простых множителей числа N
     """
     simpl_num_list(abs(n))
-    file = open('simp_num.txt', 'r')
+    file = open('simp_num', 'r')
     div_simp = file.read().split()
     file.close()
     for el in div_simp:
@@ -23,10 +23,10 @@ def simpl_num_list(num):
     Определение простое число или нет
     """
     try:
-        file = open('simp_num.txt', 'r')
+        file = open('simp_num', 'r')
     except:
-        file = open('simp_num.txt', 'w+').write("1 2 3")
-        file = open('simp_num.txt', 'r')
+        file = open('simp_num', 'w+').write("1 2 3")
+        file = open('simp_num', 'r')
 
     simp_numbers = file.read().split()
     file.close()
@@ -35,7 +35,7 @@ def simpl_num_list(num):
         for i in range(last_el + 1, abs(num) + 1):
             sim_num = find_simpl_num(i)
             if sim_num is not None:
-                file = open('simp_num.txt', 'a+')
+                file = open('simp_num', 'a+')
                 file.write(" " + str(sim_num))
                 file.close()
 
