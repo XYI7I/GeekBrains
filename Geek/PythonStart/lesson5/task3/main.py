c9b2f80973135bb81b6004740e01205c82f032cf
Greg <xyi7i@yandex.ru>
2022-07-29 20:23:38 +0300

    task3

--- Geek/PythonStart/lesson5/task3/main.py
+++ Geek/PythonStart/lesson5/task3/main.py
@@ -2,3 +2,30 @@
 Создайте программу для игры в "Крестики-нолики"
 
 """
+import pygame
+import sys
+
+pygame.init()
+size_block = 100
+margin = 15
+width = height = size_block * 3 + margin * 4
+
+size_window = (width, height)
+screen = pygame.display.set_mode(size_window)
+pygame.display.set_caption('TIC-TAC-TOE')
+black = (0, 0, 0)
+blue = (0, 0, 255)
+green = (0, 255, 0)
+white = (255, 255, 255)
+
+mas = [[0] * 3 for i in range(3)]
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+
+    for row in range(3):
+        for col in range(3):
+            x =
