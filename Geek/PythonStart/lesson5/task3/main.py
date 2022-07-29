423f4c1d294a4212db36269123d139dabac17614
Greg <xyi7i@yandex.ru>
2022-07-29 21:47:37 +0300

    task3

--- Geek/PythonStart/lesson5/task3/main.py
+++ Geek/PythonStart/lesson5/task3/main.py
@@ -5,6 +5,28 @@
 import pygame
 import sys
 
+
+def win_game(mas, sign):
+    count_zer = 0
+    diag = []
+    diag1 = []
+    for i in range(3):
+        diag.append(mas[i][i])
+        diag1.append(mas[2 - i][i])
+        if diag.count(sign) == 3 or diag1.count(sign) == 3:
+            return 'win' + sign
+    for row in mas:
+        count_zer += row.count(0)
+        if row.count(sign) == 3:
+            return 'win ' + sign
+    for col in range(3):
+        if mas[:][col] == sign:
+            return 'win ' + sign
+    if count_zer == 0:
+        return 'Piece'
+    return False
+
+
 pygame.init()
 size_block = 100
 margin = 15
@@ -17,15 +39,61 @@ black = (0, 0, 0)
 blue = (0, 0, 255)
 green = (0, 255, 0)
 white = (255, 255, 255)
+count = 0
 
 mas = [[0] * 3 for i in range(3)]
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT and not game_over:
             pygame.quit()
             sys.exit(0)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x_mouse, y_mouse = pygame.mouse.get_pos()
 
-    for row in range(3):
-        for col in range(3):
-            x =
+            col = x_mouse // (size_block + margin)
+            row = y_mouse // (size_block + margin)
+
+            if mas[row][col] == 0:
+                if count % 2 == 0:
+                    mas[row][col] = 'X'
+                else:
+                    mas[row][col] = 'O'
+                count += 1
+
+    if not game_over:
+        for row in range(3):
+            for col in range(3):
+                if mas[row][col] == 'X':
+                    color = blue
+
+                elif mas[row][col] == 'O':
+                    color = green
+                else:
+                    color = white
+                x = col * size_block + (col + 1) * margin
+                y = row * size_block + (row + 1) * margin
+                pygame.draw.rect(screen, color, (x, y, size_block, size_block))
+                if mas[row][col] == 'X':
+                    pygame.draw.line(screen, green, (x + size_block / 4, y + size_block / 4),
+                                     (x + size_block * 3 / 4, y + size_block * 3 / 4), 10)
+                    pygame.draw.line(screen, green, (x + size_block / 4, y + size_block * 3 / 4),
+                                     (x + size_block * 3 / 4, y + size_block / 4), 10)
+                elif mas[row][col] == 'O':
+                    pygame.draw.circle(screen, blue, (x + size_block / 2, y + size_block / 2), size_block * 1.27 / 4, 10)
+
+    if (count - 1) % 2 == 0:
+        game_over = win_game(mas, 'X')
+    else:
+        game_over = win_game(mas, 'O')
+
+    if game_over:
+        screen.fill(black)
+        font = pygame.font.SysFont('Monserrat', 77)
+        text1 = font.render(game_over, True, white)
+        test_rect = text1.get_rect()
+        text_x = screen.get_width() / 2 - test_rect.width / 2
+        text_y = screen.get_height() / 2 - test_rect.height / 2
+        screen.blit(text1, [text_x, text_y])
+
+    pygame.display.update()
