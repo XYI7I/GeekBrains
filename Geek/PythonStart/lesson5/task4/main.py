3adc5ffd7bf5f34e79677aebcd4dbf19f8a358bf
Greg <xyi7i@yandex.ru>
2022-07-29 22:53:16 +0300

    task4

--- Geek/PythonStart/lesson5/task4/main.py
+++ Geek/PythonStart/lesson5/task4/main.py
@@ -5,23 +5,36 @@
 
 
 def encode_rle(s):
-    encoding = ""  # сохраняет выходную строку
+    """
+    Функция RLE алгоритма сжатия данных.
+    """
+    encoding = ""
 
     i = 0
     while i < len(s):
-        # подсчитывает количество вхождений символа в индексе `i`
         count = 1
 
         while i + 1 < len(s) and s[i] == s[i + 1]:
             count = count + 1
             i = i + 1
-
-        # добавляет к результату текущий символ и его количество
         encoding += str(count) + s[i]
         i = i + 1
 
     return encoding
 
+def decode_rle(rle_data):
+    """
+    Функция RLE алгоритма восстановления данных.
+    """
+    data = ""
+
+    for i in range(0, len(rle_data), 2):
+        data += rle_data[i + 1]*int(rle_data[i])
+    return data
+
 
 text_data = 'Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных'
 print(encode_rle(text_data))
+rle_data = encode_rle(text_data)
+
+print(decode_rle(rle_data))
