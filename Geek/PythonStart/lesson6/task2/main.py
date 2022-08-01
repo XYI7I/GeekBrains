83c809d7192fd7076c392a663c39468916f49b10
XYI7I <xyi7i@yandex.ru>
2022-08-01 12:55:02 +0300

    task2

--- Geek/PythonStart/lesson6/task2/main.py
+++ Geek/PythonStart/lesson6/task2/main.py
@@ -12,7 +12,12 @@
 Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 """
 
+
 def find_uniq_el(list):
+    """
+    Функция позволяет получить список уникальных элементов заданной последовательности.
+    Пример: [1, 2, 3, 5, 1, 5, 3, 10] => [2, 10]
+    """
     max_elem = max(list) + 1
     count = [0] * max_elem
 
@@ -26,14 +31,15 @@ def find_uniq_el(list):
     return result
 
 
-
-
 list_num = [1, 2, 3, 5, 1, 5, 3, 10]
-list_unq1 = [el for el in list_num if list_num.count(el) == 1]
 print(list_num)
+
 list_unq = find_uniq_el(list_num)
 print(list_unq)
+
+list_unq1 = [el for el in list_num if list_num.count(el) == 1]
 print(list_unq1)
 
-
-
+# Найти сумму чисел списка стоящих на нечетной позиции
+sum_el = sum([el for el in list_num[::2]])
+print(f'Сумма чисел списка {list_num} стоящих на нечетной позициях = {sum_el}')
