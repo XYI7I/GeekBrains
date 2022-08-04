31f2520f0943b0cedf2302d5358c98d0f1e22736
Greg <xyi7i@yandex.ru>
2022-08-04 11:19:50 +0300

    task2

--- Geek/PythonStart/lesson5/task2/main.py
+++ Geek/PythonStart/lesson5/task2/main.py
@@ -8,6 +8,44 @@ a) Добавьте игру против бота
 b) Подумайте как наделить бота ""интеллектом""
 """
 
-Доделываю!
+from random import randint
+
+candy = 280
+query = randint(1, 7) % 2
+turn_candy = 28
+
+while candy > 28:
+    # print(randint(1,3))
+    if query == 0:
+        print('The first player\'s turn')
+        query += 1
+        turn_candy = 29
+        while turn_candy > 28 or turn_candy < 1:
+            turn_candy = int(input('take candies in range(1:28) - '))
+    else:
+        # print(candy//28, candy//29)
+        if 29 < candy < 58:
+            turn_candy = candy - 29
+        elif candy > 29 * (candy // 29):
+            # print(candy, 29*(candy//29))
+            turn_candy = candy - 29 * (candy // 29)
+        # print('1')
+        elif candy < 29 * (candy // 29):
+            turn_candy = candy - 29 * (candy // 28 - 1)
+        elif candy == 58:
+            print('You win leather bag!')
+            exit()
+        else:
+            turn_candy = randint(1, 28)
+        print(f'The bot turn - {turn_candy} candies')
+        query -= 1
+
+    candy -= turn_candy
+    print(f"{candy} - candies left.")
+
+if query == 0:
+    print('You win leather bag!')
+else:
+    print('The bot player win!')
 
 
