98e58a36e6e77fe88a8228c453174fa1eac252ea
XYI7I <xyi7i@yandex.ru>
2022-08-06 10:31:01 +0300

    task1

--- Geek/PythonStart/lesson7/task1/main.py
+++ Geek/PythonStart/lesson7/task1/main.py
@@ -63,7 +63,6 @@ def convert_to_txt(data_frame):
     print('test')
 
 
-
 df = pd.read_csv('data.csv', ';', encoding='windows-1251')
 data_val = df.loc[df['name'] == 'Имя_1']['phone']
 print(data_val)
