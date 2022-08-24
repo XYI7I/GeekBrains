199d1de180f66920bdcd5b5fefd2cd96c5e5e403
Greg <xyi7i@yandex.ru>
2022-08-24 16:03:57 +0300

    task3

---
+++ Geek/PythonStart/lesson9/task3/main.py
@@ -0,0 +1,12 @@
+import pytube
+
+try:
+    youtube_video = pytube.YouTube('https://youtu.be/x5oGW6kv08A?list=RDx5oGW6kv08A')
+
+    filters = youtube_video.streams.filter(progressive=True, file_extension='mp4')
+
+    # download the highest quality video
+    filters.get_highest_resolution().download()
+    print('Video Downloaded Successfully')
+except Exception as e:
+    print(e)
