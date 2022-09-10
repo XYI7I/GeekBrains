88245a448134f36d6b9ffe3780aff887ec0bb9fe
XYI7I <xyi7i@yandex.ru>
2022-09-10 09:08:28 +0300

    task2

--- Geek/JavaStart/lesson1/task1/task1/src/Main.java
+++ Geek/JavaStart/lesson1/task1/task1/src/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Реализовать функцию возведения числа а в степень b. a, b ∈ Z.
@@ -8,7 +10,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Программа возводит число в любую целочисленную степень.");
         int numa = input_num();
+        file_wr(String.valueOf(numa), "input.txt", "a");
         int numb = input_num();
+        file_wr(String.valueOf(numb), "input.txt", "b");
+        file_wr(String.valueOf(pow_num(numa, numb)), "output.txt", "a^b = ");
         System.out.printf("%s^%s = %s", numa, numb, pow_num(numa, numb));
     }
 
@@ -41,6 +46,16 @@ public class Main {
             var = 1 / var;
         }
         return var;
+    }
+
+    static void file_wr(String input, String filename, String var) {
+        try (FileWriter fw = new FileWriter(filename, true)) {
+            fw.write(var + " " + input);
+            fw.append('\n');
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
     }
 
 }
