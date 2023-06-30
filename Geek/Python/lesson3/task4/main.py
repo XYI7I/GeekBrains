1a127aabc81f0a88589d25cc94931b453b3fcc57
Greg <xyi7i@yandex.ru>
2023-06-30 11:25:44 +0300

    lesson3

--- Geek/Python/lesson3/task4/main.py
+++ Geek/Python/lesson3/task4/main.py
@@ -1,35 +1,49 @@
 """
-Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
-Программа должна возвращать сумму и произведение* дробей.
-Для проверки своего кода используйте модуль fractions.
+Создайте словарь со списком вещей для похода в качестве ключа и их массой в качестве значения.
+Определите какие вещи влезут в рюкзак передав его максимальную грузоподъёмность.
+Достаточно вернуть один допустимый вариант.
+*Верните все возможные варианты комплектации рюкзака.
 """
 
-from fractions import Fraction
+
+def find_items_for_backpack(items, max_weight):
+    # Инициализация матрицы для хранения результатов
+    dp = [[0] * (max_weight + 1) for _ in range(len(items) + 1)]
+
+    # Заполнение матрицы значениями
+    for i in range(1, len(items) + 1):
+        item_weight = items[i - 1]["weight"]
+        item_value = items[i - 1]["value"]
+        for j in range(1, max_weight + 1):
+            if item_weight <= j:
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - item_weight] + item_value)
+            else:
+                dp[i][j] = dp[i - 1][j]
+
+    # Восстановление выбранных предметов
+    selected_items = []
+    i, j = len(items), max_weight
+    while i > 0 and j > 0:
+        if dp[i][j] != dp[i - 1][j]:
+            selected_items.append(items[i - 1])
+            j -= items[i - 1]["weight"]
+        i -= 1
+
+    return selected_items
 
 
-def calculate_fraction_operations(fraction1, fraction2):
-    # Разделяем строки дробей на числитель и знаменатель
-    numerator1, denominator1 = map(int, fraction1.split('/'))
-    numerator2, denominator2 = map(int, fraction2.split('/'))
+# Пример использования
+items = [
+    {"name": "Sleeping Bag", "weight": 3, "value": 8},
+    {"name": "Tent", "weight": 4, "value": 10},
+    {"name": "Water Bottle", "weight": 1, "value": 4},
+    {"name": "Food", "weight": 2, "value": 6},
+]
 
-    # Создаем объекты Fraction для каждой дроби
-    fraction_obj1 = Fraction(numerator1, denominator1)
-    fraction_obj2 = Fraction(numerator2, denominator2)
+max_weight = 6
 
-    # Выполняем операции с дробями
-    sum_fraction = fraction_obj1 + fraction_obj2
-    product_fraction = fraction_obj1 * fraction_obj2
+selected_items = find_items_for_backpack(items, max_weight)
 
-    return sum_fraction, product_fraction
-
-
-# Получаем две строки с дробями от пользователя
-fraction1 = input("Введите первую дробь (в формате a/b): ")
-fraction2 = input("Введите вторую дробь (в формате a/b): ")
-
-# Выполняем вычисления
-sum_result, product_result = calculate_fraction_operations(fraction1, fraction2)
-
-# Выводим результаты
-print("Сумма дробей:", sum_result)
-print("Произведение дробей:", product_result)
+print("Вещи, которые поместятся в рюкзак:")
+for item in selected_items:
+    print(item["name"])
