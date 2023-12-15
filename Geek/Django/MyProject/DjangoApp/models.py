5f19be482945a21ed1f388a9ca3c91a857df7dda
XYI7I <xyi7i@yandex.ru>
2023-12-15 18:38:23 +0300

    lesson2

--- Geek/Django/MyProject/DjangoApp/models.py
+++ Geek/Django/MyProject/DjangoApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 from django.db import models
-import Pillow
+
 
 class Client(models.Model):
     name = models.CharField(max_length=255)
@@ -15,6 +15,7 @@ class Client(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
@@ -24,6 +25,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Order(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
