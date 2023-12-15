7798006a974b10755989020d0100ac819abc2fce
XYI7I <xyi7i@yandex.ru>
2023-12-15 18:25:55 +0300

    lesson2

--- Geek/Django/MyProject/DjangoApp/models.py
+++ Geek/Django/MyProject/DjangoApp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 from django.db import models
+import Pillow
 
 class Client(models.Model):
     name = models.CharField(max_length=255)
@@ -29,6 +30,7 @@ class Order(models.Model):
     products = models.ManyToManyField(Product)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     order_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)
 
     def __str__(self):
         return f"Order #{self.id} by {self.client.name}"
