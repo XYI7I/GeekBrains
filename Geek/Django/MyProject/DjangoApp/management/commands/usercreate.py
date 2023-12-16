0f445a051bcd56572f5444b2bd43d55e282d547f
XYI7I <xyi7i@yandex.ru>
2023-12-16 17:13:00 +0300

    lesson2

--- Geek/Django/MyProject/DjangoApp/management/commands/usercreate.py
+++ Geek/Django/MyProject/DjangoApp/management/commands/usercreate.py
@@ -6,6 +6,7 @@ class Command(BaseCommand):
     help = "Create user."
 
     def handle(self, *args, **kwargs):
-        user = User(name='John', email='john@example.com', password='secret', age=25)
+        # user = User(name='John', email='john@example.com', password='secret', age=25)
+        user = User(name='XYI7I', email='xyi7i@example.com', password='secret', age=33, phone_number='123456789', address='123 Main St')
         user.save()
         self.stdout.write(f'{user}')
